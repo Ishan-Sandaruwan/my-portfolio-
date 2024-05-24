07b174301c72812b1ead90dfe35bf2aa6d1b459a
W I Sandaruwan <sadaruwan0427@gmail.com>
2024-05-24 11:41:51 +0530

    complete hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="border-b border-neutral-700 pb-4 lg:mb-36">
+      <div className="flex flex-wrap items-center">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              Ishan Sandaruwan
+            </h1>
+            <span className="bg-gradient-to-tr from-pink-300 via-slate-400 to-purple-500 bg-clip-text tracking-tight text-transparent">
+              Full Stack Developer
+            </span>
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              Hi, I'm Ishan Sandaruwan, a passionate Full Stack Developer with a
+              talent for crafting dynamic and responsive web applications. With
+              a deep understanding of both front-end and back-end technologies,
+              I thrive on transforming complex challenges into seamless,
+              user-friendly solutions. Let's build something amazing together!
+            </p>
+          </div>
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8 ">
+          <div className="flex justify-center ">
+            <img src="./profile.jpeg" alt="Ishan Sandaruwan" className=" object-cover brightness-50 hover:brightness-100 transition-colors cursor-pointer" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { FaFacebook } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className="mb-20 flex items-center justify-between py-6">
+    <div className="mb-12 flex items-center justify-between py-6">
       <div className="text-4xl ">Is</div>
       <div className="mx-8 flex items-center justify-center gap-4 text-3xl">
         <FaLinkedin />
